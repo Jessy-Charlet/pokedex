f09716a34a053a32f6aac6d0ea4190d3e5594c00
Jessy Charlet <Jessycharlet@gmail.com>
2024-06-12 11:00:40 +0200

    Fonction de chargement et MissingNo

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -5,95 +5,100 @@ import { RouterLink } from '@angular/router';
 import { PokemonInfoService } from '../../services/pokemon.info.service';
 
 @Component({
-  selector: 'app-pokemon-details',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './pokemon-details.component.html',
-  styleUrl: './pokemon-details.component.css'
+   selector: 'app-pokemon-details',
+   standalone: true,
+   imports: [RouterLink],
+   templateUrl: './pokemon-details.component.html',
+   styleUrl: './pokemon-details.component.css'
 })
 export class PokemonDetailsComponent {
-  pokemon: Pokemon | null = null;
+   pokemon: Pokemon | null = null;
+   time: number = 0;
 
 
-  @Input() set id(pokemonId: number) {
-    this.api.getPokemonsById(pokemonId).then(pokemon => this.pokemon = pokemon)
-      .catch(error => console.log(error));
-  }
+   @Input() set id(pokemonId: number) {
+      this.time = 0;
+      this.api.getPokemonsById(pokemonId).then(pokemon => this.pokemon = pokemon)
+         .catch(error => console.log(error));
+         setTimeout(() =>{
+            this.time = 1;
+         },500);
+   }
 
-  public log(val: any) { console.log(val) }
+   public log(val: any) { console.log(val) }
 
-  constructor(private api: ApiService) { }
+   constructor(private api: ApiService) { }
 
-  public colorType(pokemon: Pokemon) {
-    let color = [];
-    for (let type of pokemon.apiTypes) {
-       switch (type.name) {
-          case "Normal":
-             color.push("#7c8384");
-             break;
-          case "Feu":
-             color.push("#fea421");
-             break;
-          case "Plante":
-             color.push("#3ab253");
-             break;
-          case "Eau":
-             color.push("#59b0db");
-             break;
-          case "Sol":
-             color.push("#cc783d");
-             break;
-          case "Combat":
-             color.push("#d25164");
-             break;
-          case "Vol":
-             color.push("#728cda");
-             break;
-          case "Poison":
-             color.push("#e158cc");
-             break;
-          case "Roche":
-             color.push("#c9b966");
-             break;
-          case "Insecte":
-             color.push("#7fbf00");
-             break;
-          case "Spectre":
-             color.push("#877bcf");
-             break;
-          case "Acier":
-             color.push("#2b8796");
-             break;
-          case "\u00c9lectrik":
-             color.push("#ffd13d");
-             break;
-          case "Psy":
-             color.push("#ff7166");
-             break;
-          case "Glace":
-             color.push("#6ed6cc");
-             break;
-          case "Dragon":
-             color.push("#1d6bbc");
-             break;
-          case "T\u00e9n\u00e8bres":
-             color.push("#545162");
-             break;
-          case "F\u00e9e":
-             color.push("#f18cd7");
-             break;
-          default:
-             color.push("sans type");
-             break;
-       }
-    }
-    return color;
- }
-   public statsRange(value : number){
+   public colorType(pokemon: Pokemon) {
+      let color = [];
+      for (let type of pokemon.apiTypes) {
+         switch (type.name) {
+            case "Normal":
+               color.push("#7c8384");
+               break;
+            case "Feu":
+               color.push("#fea421");
+               break;
+            case "Plante":
+               color.push("#3ab253");
+               break;
+            case "Eau":
+               color.push("#59b0db");
+               break;
+            case "Sol":
+               color.push("#cc783d");
+               break;
+            case "Combat":
+               color.push("#d25164");
+               break;
+            case "Vol":
+               color.push("#728cda");
+               break;
+            case "Poison":
+               color.push("#e158cc");
+               break;
+            case "Roche":
+               color.push("#c9b966");
+               break;
+            case "Insecte":
+               color.push("#7fbf00");
+               break;
+            case "Spectre":
+               color.push("#877bcf");
+               break;
+            case "Acier":
+               color.push("#2b8796");
+               break;
+            case "\u00c9lectrik":
+               color.push("#ffd13d");
+               break;
+            case "Psy":
+               color.push("#ff7166");
+               break;
+            case "Glace":
+               color.push("#6ed6cc");
+               break;
+            case "Dragon":
+               color.push("#1d6bbc");
+               break;
+            case "T\u00e9n\u00e8bres":
+               color.push("#545162");
+               break;
+            case "F\u00e9e":
+               color.push("#f18cd7");
+               break;
+            default:
+               color.push("sans type");
+               break;
+         }
+      }
+      return color;
+   }
+   public statsRange(value: number) {
       let stats = [];
-      const result : number = value / 220 * 100;
+      const result: number = value / 220 * 100;
       stats.push(result);
-      if (result < 25){
+      if (result < 25) {
          stats.push('#d25164');
       } else if (result < 50) {
          stats.push('#ffd13d');
@@ -102,4 +107,5 @@ export class PokemonDetailsComponent {
       }
       return stats;
    }
+
 }
